01df1f694787d4899c5c0d77345da03c43888aa5
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-15 18:08:39 -0300

    Aula 3 - Strings

--- aula3.py
+++ aula3.py
@@ -3,5 +3,18 @@ Python = Linguagem de Programação
 Tipo de tipagem = Dinâmica / Fortemente tipada
 str -> string -> textos
 Strings são textos entre aspas simples ou duplas.
-
 """
+
+# Aspas simples
+print('Olá, Willian')
+
+# Aspas duplas
+print("Olá, Otávio!")
+
+# Ecape \
+print('\'Olá, Willian\'')
+print("Olá, Willian \"Otávio\"")
+
+# r
+print(r'\'Olá, Willian\'')
+
