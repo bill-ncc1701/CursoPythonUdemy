f4e75f52e559077367bd7dc802d25c263fde0fda
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-15 16:50:04 -0300

    Aula 1

---
+++ aula1.py
@@ -0,0 +1,15 @@
+# Permite escrever um comentário
+print(123) # Na frente
+
+# Abaixo
+print("Hello World")
+
+
+"""
+Este é um comentário de várias linhas.
+Chamado de Docstring.
+
+Cuidado! O Interpretador lê isso.
+
+"""
+
